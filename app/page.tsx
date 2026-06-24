b0705c918e92fedfc5b1da945c2085bf277d836b
NicholasC2 <nicholasc2@proton.me>
2026-06-25 11:43:39 +1200

    pulled from template

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "next/image";
+
+import { TButton, TInput, TNavbar } from "@t-apps/ui"
+
+export default function Home() {
+    return (
+		<>
+    
+		</>
+    );
+}
